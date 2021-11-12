c49ac5cb508e22164684eb80d07a1dac6648597e
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2021-11-12 20:25:51 -0300

    feat: add repository listing

--- routes.js
+++ routes.js
@@ -4,17 +4,33 @@ import got from 'got'
 
 const routes = Router()
 
-routes.get('/users/:profileUrl', (req, res) => {
+class Repository {
+    constructor(full_name, description) {
+        this.full_name = full_name,
+        this.description = description
+    }
+}
+
+routes.get('/users/:username', (req, res) => {
     (async () => {
         try {
-            const response = await got(`https://api.github.com/users/gabrielfmagalhaes/repos`)
-        
-            console.log(response.body)
+            const { body } = await got(`https://api.github.com/users/${req.params.username}/repos`)
+            
+            const parseBody = JSON.parse(body)
+
+            const repositories = []
+
+            parseBody.forEach(element => {
+                const { full_name, description } = element 
+
+                repositories.push(new Repository(full_name, description))
+            })            
+            
+            res.send(repositories)
         } catch (error) {
             console.log(error.response.body);
-            //=> 'Internal server error ...'
         }
-    })();
+    })()
 })
 
 export default routes
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import express from 'express'
 
 import routes from './routes.js'
 
-
 const app = express()
 const port = 3000
 
-app.use('/app-git', routes)
+app.use('/app-git-repositories', routes)
 
 app.listen(port, () => {
     console.log(`Server launched at port ${port} 🚀`)
